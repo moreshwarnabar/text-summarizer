6803d5d294a1fb4d943ac805d34b722b6e9645e9
moreshwarnabar <mrnabar@gmail.com>
2024-02-21 14:16:22 -0700

    added data ingestion feature

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from text_summarizer.logging import logger
+from text_summarizer.pipeline.data_ingestion_stage import DataIngestionTrainingPipeline
 
-logger.info('Welcome to the custom log')
+STAGE_NAME = 'Data Ingestion Stage'
+try:
+    logger.info(f">>>>>> Starting {STAGE_NAME}...")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> Completed {STAGE_NAME}")
+except Exception as e:
+    logger.error(f">>>>>> Failed to complete {STAGE_NAME}: {e}")
+    raise e
