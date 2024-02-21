7649aa8ce848756ecae3b949c14b753db6eb722d
moreshwarnabar <mrnabar@gmail.com>
2024-02-21 15:47:31 -0700

    added data validation pipeline

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from text_summarizer.logging import logger
 from text_summarizer.pipeline.data_ingestion_stage import DataIngestionTrainingPipeline
+from text_summarizer.pipeline.data_validation_stage import DataValidationTrainingPipeline
 
 STAGE_NAME = 'Data Ingestion Stage'
 try:
     logger.info(f">>>>>> Starting {STAGE_NAME}...")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f">>>>>> Completed {STAGE_NAME}")
+except Exception as e:
+    logger.error(f">>>>>> Failed to complete {STAGE_NAME}: {e}")
+    raise e
+
+
+STAGE_NAME = 'Data Validation Stage'
+try:
+    logger.info(f">>>>>> Starting {STAGE_NAME}...")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
     logger.info(f">>>>>> Completed {STAGE_NAME}")
 except Exception as e:
     logger.error(f">>>>>> Failed to complete {STAGE_NAME}: {e}")
